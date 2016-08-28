0f65a162fac52e70f00bb1773aef1bf2354df15c
wbjacks <william.b.jackson.14@dartmouth.edu>
2016-08-28 01:20:05 -0700

    Added crappy comparison, will move to a service

--- src/main/java/com/wbjacks/App.java
+++ src/main/java/com/wbjacks/App.java
@@ -1,7 +1,47 @@
 package com.wbjacks;
 
+import com.wbjacks.models.Card;
+import com.wbjacks.models.Card.Rank;
+import com.wbjacks.models.Card.Suite;
+import com.wbjacks.models.Hand;
+import com.wbjacks.models.services.HandClassifierService;
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Hand hand1 = new Hand(new ArrayList() {{
+                add(new Card(Rank.TWO, Suite.HEART));
+                add(new Card(Rank.TWO, Suite.DIAMOND));
+                add(new Card(Rank.TWO, Suite.DIAMOND));
+                add(Card.joker());
+                add(new Card(Rank.THREE, Suite.SPADE));
+            }});
+            Hand hand2 = new Hand(new ArrayList() {{
+                add(new Card(Rank.SIX, Suite.SPADE));
+                add(new Card(Rank.SIX, Suite.HEART));
+                add(new Card(Rank.SIX, Suite.DIAMOND));
+                add(new Card(Rank.KING, Suite.CLUB));
+                add(new Card(Rank.KING, Suite.HEART));
+            }});
+
+            HandClassifierService handClassifierService = new HandClassifierService();
+            hand1.setHandClassification(handClassifierService.classifyHand(hand1));
+            hand2.setHandClassification(handClassifierService.classifyHand(hand2));
+
+            // This is awful
+            if (hand1.compareTo(hand2) == 1) {
+                System.out.println("Hand 1 wins!");
+            }
+            else if (hand1.compareTo(hand2) == -1) {
+                System.out.println("Hand 2 wins!");
+            }
+            else if (hand1.compareTo(hand2) == 0) {
+                System.out.println("They tie");
+            }
+        }
+        catch (Hand.TooManyCardsException ignored) {}
+        catch (Hand.IncompleteHandException ignored) {}
     }
 }
--- src/main/java/com/wbjacks/models/Hand.java
+++ src/main/java/com/wbjacks/models/Hand.java
@@ -2,10 +2,12 @@ package com.wbjacks.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
     public static final int NUMBER_OF_CARDS_IN_COMPLETE_HAND = 5;
     private final List<Card> _cards = new ArrayList<>();
+    private HandClassification _handClassification;
 
     // TODO: (wbjacks) assert maximum size?
     public Hand(List<Card> cards) throws TooManyCardsException {
@@ -26,15 +28,50 @@ public class Hand {
         return _cards;
     }
 
+    public HandClassification getHandClassification() {
+        return _handClassification;
+    }
+
+    public void setHandClassification(HandClassification handClassification) {
+        _handClassification = handClassification;
+    }
+
+    @Override
+    public int compareTo(Hand hand) {
+        int classificationCompareValue = Integer.compare(this.getHandClassification().getClassification().getValue(),
+                hand.getHandClassification().getClassification().getValue());
+        if (classificationCompareValue == 0) {
+            // Tie, compare highest cards
+            try {
+                List<Integer> thisValueRankings = this.getCards().stream().map(card -> card.getRank().getValue())
+                        .sorted().collect(Collectors.toList());
+                List<Integer> thatValueRankings = hand.getCards().stream().map(card -> card.getRank().getValue())
+                        .sorted().collect(Collectors.toList());
+                for (Integer thisValue : thisValueRankings) {
+                    int compareValue = Integer.compare(thisValue, thatValueRankings.remove(0));
+                    if (compareValue != 0) {
+                        return compareValue;
+                    }
+                }
+                return 0;
+            } catch (IncompleteHandException e) {
+                return 0; // TODO: (wbjacks) Log error
+            }
+        }
+        return classificationCompareValue;
+    }
+
     public static class TooManyCardsException extends Exception {
         public TooManyCardsException() {
-            super("Attempt to add more cards than the maximum allowed in a single hand. Maximum number of allowed cards is: " + NUMBER_OF_CARDS_IN_COMPLETE_HAND);
+            super("Attempt to add more cards than the maximum allowed in a single hand. Maximum number of allowed " +
+                    "cards is: " + NUMBER_OF_CARDS_IN_COMPLETE_HAND);
         }
     }
 
     public static class IncompleteHandException extends Exception {
         public IncompleteHandException() {
-            super(String.format("Attempting to access cards in an incomplete hand. A complete hand contains %d cards.", NUMBER_OF_CARDS_IN_COMPLETE_HAND));
+            super(String.format("Attempting to access cards in an incomplete hand. A complete hand contains %d cards"
+                    + ".", NUMBER_OF_CARDS_IN_COMPLETE_HAND));
         }
     }
 }
